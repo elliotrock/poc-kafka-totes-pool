dff683646f7caf946612566f6407119f58e95864
Elliot Rock <elliot.rock@192-168-1-105.tpgi.com.au>
2022-05-30 16:30:41 +1000

    feat(install.sh): Added a lower level shellscript install. update(makefile): Removed duplicate commands now in the install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# install for various dependencies
+# homebrew
+echo "check homebrew"
+if ! command -v brew 
+then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+# makefile
+if ! command -v makefile 
+then brew install make
+fi
+
+if ! command -v jq
+then brew install jq
+fi 
